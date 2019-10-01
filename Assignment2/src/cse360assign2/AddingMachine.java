91e4304623df90c989b7e0a9c66deb7c76bc94ca
Matteo Chirco <mchirco@asu.edu>
2019-10-01 13:58:58 -0700

    Second version

--- Assignment2/src/cse360assign2/AddingMachine.java
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -1,30 +1,45 @@
+
 package cse360assign2;
+
+/**
+ * This is a class for Assignment 2
+ * The class will perform operations using the add/subtract operands.
+ * Each operation is saved to the history, which can be called using toString().
+ * The total method will return the total of the calculations.
+ * 
+ * https://github.com/mchirco/Assignment2.git
+ * @author Matteo Chirco
+ */
 
 public class AddingMachine {
 
 	private int total;
+	private String history = "0";
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total += value;
+		history = history + " + " + value;
 	}
 	
 	public void subtract (int value) {
-		
+		total-= value;
+		history = history + " - " + value;
 	}
 		
 	public String toString () {
-		return "";
+		return history;
 	}
 
 	public void clear() {
-	
+		total = 0;
+		history = "0";
 	}
 }
--- Assignment2/src/cse360assign2/Assign2Test.java
+++ Assignment2/src/cse360assign2/Assign2Test.java
@@ -8,8 +8,15 @@ class Assign2Test {
 
 	@Test
 	void test() {
-		System.out.println("Hello world!");
-		fail("Not yet implemented");
+	AddingMachine ad = new AddingMachine();
+	ad.add(4);
+	ad.subtract(2);
+	ad.add(5);
+	
+	System.out.println("toString: " + ad.toString());
+	System.out.println("Total: " + ad.getTotal());
+
+	
 	}
 
 }
