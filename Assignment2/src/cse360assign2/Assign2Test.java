58bc57c113379f7b21bf558ca7b1730f9c8d0342
Matteo Chirco <mchirco@asu.edu>
2019-10-01 13:28:21 -0700

    Testing for initial commit. 123.

---
+++ Assignment2/src/cse360assign2/Assign2Test.java
@@ -0,0 +1,15 @@
+package cse360assign2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Assign2Test {
+
+	@Test
+	void test() {
+		System.out.println("Hello world!");
+		fail("Not yet implemented");
+	}
+
+}
